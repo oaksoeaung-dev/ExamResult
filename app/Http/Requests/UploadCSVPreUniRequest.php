d29c59f7a8eede273b889b3fa23e771b60e2e5de
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2024-10-04 12:24:12 +0630

    Update controller

---
+++ app/Http/Requests/UploadCSVPreUniRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UploadCSVPreUniRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+    public function rules(): array
+    {
+        return [
+            "csv" => "file|required|mimes:csv,txt",
+        ];
+    }
+}
